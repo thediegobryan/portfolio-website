aac25ec0a9465d1a58dc38de7860af9e097b3f0c
Diego Bryan <thediegobryan@gmail.com>
2024-04-11 09:32:27 -0400

    fixed sending email address

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.com>",
+      from: "Contact Form <onboarding@resend.dev>",
       to: "diegobryanre@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail as string,
