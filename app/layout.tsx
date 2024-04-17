8d1eaafe935840cfed7c4fd103502d94cc1b75f9
Diego Bryan <thediegobryan@gmail.com>
2024-04-17 09:10:58 -0400

    added vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Header from "@/components/header";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -5,7 +6,6 @@ import "./globals.css";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,16 +21,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body suppressHydrationWarning={true} className={`${inter.className} bg-stone-50 text-gray-950 relative pt-28 sm:pt-36`}>
+      <body
+        suppressHydrationWarning={true}
+        className={`${inter.className} bg-stone-50 text-gray-950 relative pt-28 sm:pt-36`}
+      >
         {/* <div className="bg-[#efcfce] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#e4d0c3] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div> */}
         <ActiveSectionContextProvider>
-
           <Header />
           {children}
-          <Toaster position="top-right"/>
+          <Toaster position="top-right" />
           <Footer />
         </ActiveSectionContextProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
