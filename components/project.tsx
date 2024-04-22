2206e4d4f0a656b25762348ccbe26d3082d56a4f
Diego Bryan <thediegobryan@gmail.com>
2024-04-22 12:18:45 -0400

    added images to mobile

--- components/project.tsx
+++ components/project.tsx
@@ -36,7 +36,12 @@ export default function Project({
       <Link href={websiteUrl}>
         <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden rounded-lg sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 group-even:pl-4 hover:bg-gray-200 transition">
           <div className="pt-4 pb-7 px-5 group-even:pl-1 sm:group-even:pl-8 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-            <h3 className="text-2xl font-semibold">{title}</h3>
+            <h3 className="text-2xl text-center md:text-left font-semibold">
+              {title}
+            </h3>
+            <div className="h-[175px] overflow-hidden rounded-lg md:hidden mt-2">
+              <Image src={imageUrl} alt="Project I worked on" quality={95} />
+            </div>
             <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
             <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
               {tags.map((tag, index) => (
